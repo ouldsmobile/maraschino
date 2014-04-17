c2334a9aef25472d77e74def414f3d8e57a802bd
gugahoi <guga@maurorisch.com.br>
2014-04-17 16:11:06 +1000

    Fix for webroot images

--- modules/plex.py
+++ modules/plex.py
@@ -1,5 +1,6 @@
 from flask import render_template, jsonify, send_file
-from maraschino import app, logger, RUNDIR
+from jinja2.filters import FILTERS
+from maraschino import app, logger, RUNDIR, WEBROOT
 from maraschino.tools import requires_auth, get_setting_value
 from maraschino.models import PlexServer as dbserver
 from plexLib import PlexServer
@@ -22,6 +23,12 @@ def getActiveServer():
     selected_server = get_setting_value('active_server')
     server = dbserver.query.filter(dbserver.id == selected_server).first()
     return PlexServer(ip=server.localAddresses, token=server.token), server
+
+
+def plex_image(path):
+    return '%s/xhr/plex/img%s' % (WEBROOT, path)
+
+FILTERS['plex_img'] = plex_image
 
 
 @app.route('/xhr/plex/')
@@ -62,6 +69,7 @@ def xhr_recent_movies(title, id=None):
                 if type in section:
                     id=s.sections[section]['sections'][0]['key']
                     break
+
 
         recentlyAdded = p.recentlyAdded(section=id, params="X-Plex-Container-Start=0&X-Plex-Container-Size=5")
 
